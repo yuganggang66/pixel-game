0691da6ef45cf17fc3840b24b3cb261abdb457d8
ganggang.yu <ganggang.yu@mbc02k74wlq6lr.fareast.nevint.com>
2026-03-04 14:08:03 +0800

    chore: Remove base path for Vercel root deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
   // @ts-expect-error type mismatch between vite plugin and vitest
   plugins: [react()],
   test: {
