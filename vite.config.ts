95af45c94b74db0ac0d57869272fd7363cd42e95
Ricky yu <yuganggang66@gmail.com>
2026-03-06 13:09:16 +0800

    fix: [KAN-13] fix deployment base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/pixel-game/' : '/',
   // @ts-expect-error type mismatch between vite plugin and vitest
   plugins: [react()],
   test: {
